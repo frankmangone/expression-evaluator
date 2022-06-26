c8a55b30e011dc265da5cc5e9d3d0ddf5349a056
Franco Mangone <franco.mangone@agrotoken.io>
2022-06-26 16:58:43 -0300

    Configures jest to run with typescript

--- tests/parse-expression.spec.ts
+++ tests/parse-expression.spec.ts
@@ -1,7 +1,7 @@
 import parseExpression from '../src/parse-expression'
 
 describe('[func] parseExpression', () => {
-  it('Feature: parses very simple expressions', () => {
+  describe('Feature: parses very simple expressions', () => {
     it('Scenario: parses the expression: "2+p-m"', () => {
       const tokens = parseExpression('2+p-m')
       expect(tokens).toEqual(['2', '+', 'p', '-', 'm'])
