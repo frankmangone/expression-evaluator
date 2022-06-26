5001c3cbde7b0b4009ecbb231391948d9ced4c3b
Franco Mangone <franco.mangone@agrotoken.io>
2022-06-26 16:55:13 -0300

    Adds a very basic test to start doing some TDD

--- tests/parse-expression.spec.ts
+++ tests/parse-expression.spec.ts
@@ -1,5 +1,10 @@
+import parseExpression from '../src/parse-expression'
+
 describe('[func] parseExpression', () => {
-  it('works', () => {
-    expect(true).toBe(true)
+  it('Feature: parses very simple expressions', () => {
+    it('Scenario: parses the expression: "2+p-m"', () => {
+      const tokens = parseExpression('2+p-m')
+      expect(tokens).toEqual(['2', '+', 'p', '-', 'm'])
+    })
   })
 })
