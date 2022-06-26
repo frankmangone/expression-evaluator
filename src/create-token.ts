3badaacb182c221dd96c9f5b7b459fe30be6dd4f
Franco Mangone <franco.mangone@agrotoken.io>
2022-06-26 17:48:54 -0300

    Modifies regex for params so that it passes tests

--- src/create-token.ts
+++ src/create-token.ts
@@ -3,8 +3,19 @@ import { Token, ETokenType } from '../types/Token'
 const createToken = (value: string): Token => {
   return {
     value,
-    type: ETokenType.NUMBER,
+    type: getTokenType(value),
   }
 }
 
 export default createToken
+
+export const getTokenType = (value: string): ETokenType => {
+  if (isNumber(value)) return ETokenType.NUMBER
+  if (isText(value)) return ETokenType.PARAMETER
+  return ETokenType.OPERATOR
+}
+
+const isNumber = (value: string): boolean => !Number.isNaN(parseInt(value))
+const isText = (value: string): boolean =>
+  Boolean(value.match(/^[a-z](?:_?[a-z0-9]+)*$/i))
+// const isSymbol = (char: string): boolean => Boolean(char.match(/[-^()+*.\/]/))
